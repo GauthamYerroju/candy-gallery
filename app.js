45747229dda644a10ca0674eeb96bbd8ecee6c41
Gautham Yerroju <gautham.yerroju@gmail.com>
2017-01-20 02:35:26 -0800

    Taking base path as cmd arg, adding it to req obj.

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+var argv = require('minimist')(process.argv.slice(2));
+
+var basePath;
+
+if (argv._.length == 0) {
+  basePath = __dirname;
+} else {
+  basePath = argv._[0];
+}
+console.log("Candy initialized with base path: " + basePath);
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -6,7 +17,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -23,8 +33,12 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  req.basePath = basePath;
+  next();
+});
+
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
